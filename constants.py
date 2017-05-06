05740ad9b671a367e05cc4cfb498817bb48fd4f1
Ian Cave <icave@sciencelogic.com>
2017-05-06 02:55:40 -0500

    IC. Initial pass at pulling nba/college data. Still need to find all the name edge cases.

---
+++ constants.py
@@ -0,0 +1,55 @@
+draft_url = "http://www.basketball-reference.com/draft/NBA_{0}.html"
+college_player_url = "http://www.sports-reference.com/cbb/players/{0}-{1}-{2}.html"
+
+years = [2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006]
+
+stats_header = [
+    'Year',
+    'Overall Pick',
+    'Name',
+    'COLLEGE',
+    'G',
+    'MP',
+    'FG/40',
+    'FGA/40',
+    'FG%',
+    '2P/40',
+    '2PA/40',
+    '2P%',
+    '3P/40',
+    '3PA/40',
+    '3P%',
+    'FT/40',
+    'FTA/40',
+    'FT%',
+    'TRB/40',
+    'AST/40',
+    'STL/40',
+    'BLK/40',
+    'TOV/40',
+    'PF/40',
+    'PTS/40',
+    'NBA DIVIDER',
+    'G',
+    'MP',
+    'PTS',
+    'TRB',
+    'AST',
+    'FG%',
+    '3P%',
+    'FT%',
+    'MP/G',
+    'PTS/G',
+    'TRB/G',
+    'AST/G',
+    'WS',
+    'WS/48',
+    'BPM',
+    'VORP'
+]
+
+name_translations = {
+    'Kay Felder': 'Kahlil Felder',
+    'A.J. Hammons': "AJ Hammons",
+    'Stephen Zimmerman': 'Stephen Zimmermanjr'
+}
---
+++ scraping.py
@@ -0,0 +1,172 @@
+from bs4 import BeautifulSoup, Comment
+import requests
+import os
+import constants
+import logging
+
+logging. basicConfig(filename="draft.log")
+logger = logging.getLogger("DRAFT_STATS")
+
+
+def format_nba_data(player_row):
+    return_list = list()
+    name = player_row.find(attrs={'data-stat': 'player'}).text
+    return_list.append(player_row.find(attrs={'data-stat': "pick_overall"}).text)
+    return_list.append(player_row.find(attrs={'data-stat': 'player'}).text)
+    return name, player_row
+
+
+def get_college_data(player_name, year_str, player_index):
+    try:
+        if player_name in constants.name_translations.keys():
+            name = constants.name_translations[player_name]
+        else:
+            name = player_name
+        first_last = name.split(" ")
+        url = constants.college_player_url.format(first_last[0].lower(), first_last[-1].lower(), player_index)
+        page = requests.get(url)
+        if page.status_code == 404:
+            logger.warning("no data available for: {}".format(player_name))
+            return None
+
+        college_soup = BeautifulSoup(page.text, 'lxml')
+        tables = college_soup.find_all(text=lambda text: text and isinstance(text, Comment) and 'Per 40 Minutes Table' in text)
+        table_soup = BeautifulSoup(tables[0], 'lxml')
+        stat_rows = table_soup.find('tbody').find_all('tr')
+        recent_season = stat_rows[-1]
+        if year_str[-2:] != recent_season.find(attrs={"data-stat": "season"}).text[-2:]:
+            logger.warning("BAD YEAR, TRYING {0} {1}".format(player_name, player_index+1))
+            recent_season = get_college_data(player_name, year_str, player_index+1)
+
+        # for row in stat_rows:
+        #     print(player_name, row.find(attrs={'data-stat': 'g'}))
+
+        return recent_season
+
+    except:
+        logger.error("Something went wrong with: {0}".format(player_name))
+        return None
+
+
+def format_for_csv(nba_row, college_row, year):
+    name = nba_row.find(attrs={'data-stat': 'player'}).text
+    data_row_list = list()
+
+    # Add identifying info
+    data_row_list.append(year)
+    # Format: data_row_list.append(nba_row.find(attrs={"data-stat": ""}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "pick_overall"}).text)
+    data_row_list.append(name)
+    # Not necessary right now
+    #data_row_list.append(nba_row.find(attrs={"data-stat": "college_name"}).text)
+    #data_row_list.append(nba_row.find(attrs={"data-stat": "team_id"}).text)
+
+    # Add college stats
+    # Might be None, so need to account for that
+    data_row_list.append(nba_row.find(attrs={"data-stat": "college_name"}).text.replace(",", ""))
+    if college_row is None:
+        for i in range(21):
+            data_row_list.append("")
+    else:
+        #data_row_list.append(college_row.find(attrs={"data-stat": ""}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "g"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "mp"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fga_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg_pct"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg2_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg2a_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg2_pct"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg3_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg3a_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fg3_pct"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "ft_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "fta_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "ft_pct"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "trb_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "ast_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "stl_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "blk_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "tov_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "pf_per_min"}).text)
+        data_row_list.append(college_row.find(attrs={"data-stat": "pts_per_min"}).text)
+
+    # Add NBA stats
+    data_row_list.append("NBA")
+    data_row_list.append(nba_row.find(attrs={"data-stat": "g"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "mp"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "pts"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "trb"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "ast"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "fg_pct"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "fg3_pct"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "ft_pct"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "mp_per_g"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "pts_per_g"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "trb_per_g"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "ast_per_g"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "ws"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "ws_per_48"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "bpm"}).text)
+    data_row_list.append(nba_row.find(attrs={"data-stat": "vorp"}).text)
+
+
+    return name, data_row_list
+
+
+def scrape():
+
+    # Open file, write headers
+    file = open("data.csv", 'w')
+    for header in constants.stats_header:
+        file.write(header)
+        file.write(',')
+    file.write('\n')
+
+    for year in constants.years:
+        year_str = str(year)
+        # Get list of players (and nba stats)
+        draft_page = requests.get(constants.draft_url.format(year_str))
+        soup = BeautifulSoup(draft_page.text, 'lxml')
+        draft_table = soup.find('div', class_="table_outer_container")
+        rows = draft_table.find('tbody')
+        rows_list = rows.find_all('tr')
+        nba_stats_dict = dict()
+        for row in rows_list:
+            try:
+                if not row.find(attrs={'aria-label': "Player"}):
+                    name, data = format_nba_data(row)
+                    nba_stats_dict[name] = data
+            except:
+                pass
+
+        # Get college stats
+        college_stats_dict = dict()
+        for player in nba_stats_dict:
+            college_stats_dict[player] = get_college_data(player, year_str, 1)
+
+        # append all stats
+        all_stats = list()
+        for player in nba_stats_dict:
+            name, data_list = format_for_csv(nba_stats_dict[player], college_stats_dict[player], year_str)
+            all_stats.append(data_list)
+
+        # write year's stats to file
+        for player_list in all_stats:
+            write_line = ""
+            for value in player_list:
+                write_line += str(value)
+                write_line += ","
+            write_line.rstrip(',')
+            write_line += "\n"
+            file.write(write_line)
+
+    # Cleanup
+    file.close()
+    return None
+
+
+if __name__ == "__main__":
+    if "data.csv" in os.listdir(os.getcwd()):
+        os.remove(os.getcwd() + "/data.csv")
+    scrape()
